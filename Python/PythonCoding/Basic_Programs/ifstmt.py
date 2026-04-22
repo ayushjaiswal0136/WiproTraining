299683a0d06e6cd085676a3c054f0fa392b3a638
ayushjaiswal0136 <ayushjaiswalaj0987@gmail.com>
2026-04-22 16:12:50 +0530

    saveall

---
+++ Python/PythonCoding/Basic_Programs/ifstmt.py
@@ -0,0 +1,11 @@
+#Big2
+
+num1 = int(input('Enter 1st No'))
+num2 = int(input('Enter 2nd No'))
+
+if num1 == num2:
+    print('Both is Equal')
+elif num1 > num2:
+    print(num1, 'is big')
+else:
+    print(num2, 'is big')
